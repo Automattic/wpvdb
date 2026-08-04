07924b00e9375edd02e575a9312ca83270d0b7f3
Esteban Cairol <ecairol@gmail.com>
2026-08-04 14:51:05 -0600

    Fix: Add time constants for hour and day in seconds to bootstrap.php
    - Defined HOUR_IN_SECONDS and DAY_IN_SECONDS for improved time handling in tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -420,6 +420,14 @@ if ( ! defined( 'MINUTE_IN_SECONDS' ) ) {
     define( 'MINUTE_IN_SECONDS', 60 );
 }
 
+if ( ! defined( 'HOUR_IN_SECONDS' ) ) {
+    define( 'HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS );
+}
+
+if ( ! defined( 'DAY_IN_SECONDS' ) ) {
+    define( 'DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS );
+}
+
 if ( ! defined( 'WP_DEBUG' ) ) {
     define( 'WP_DEBUG', false );
 }
